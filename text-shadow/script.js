d37bee23db7b2562b028809cf929b040730c381f
technikhil10 <technikhil10@gmail.com>
2021-06-16 04:03:00 +0530

    updated comments

--- text-shadow/script.js
+++ text-shadow/script.js
@@ -1,6 +1,8 @@
 const hero = document.querySelector('.hero')
 
 const text = hero.querySelector('h1')
+
+//Here walk variable is to create offset of 100 i.e shadow offset how much far it would go depends on this
 const walk = 100;
 
 const textShadow =  function(e){
@@ -15,21 +17,20 @@ const textShadow =  function(e){
         y = y + e.target.offsetTop
     }
 
+    //Below we create xWalk and yWalk to set the mouse pointer 
+    //value between -50 to +50 which is overall offset of 100
+
     const xWalk = Math.round((x/width*walk) - (walk/2))
     const yWalk = Math.round((y/width*walk) - (walk/2))
 
+
+    //here we set the text-shadow depending on xWalk and yWalk
     text.style.textShadow = `
         ${xWalk}px ${yWalk}px 5px #f2a68f,
         ${xWalk * -1}px ${yWalk * -1}px 5px #fa75cc
         
     ` 
-
-    console.log(xWalk,yWalk);
-
-
-
-
-
 }
 
+//add listeners
 hero.addEventListener('mousemove',textShadow)
